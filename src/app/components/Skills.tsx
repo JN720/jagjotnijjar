9a3f5d23f93aa6360105f42f3535d2b9495205c6
JN720 <112584623+JN720@users.noreply.github.com>
2023-10-04 19:39:19 -0700

    fixed redis badge

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -34,7 +34,7 @@ const skills: number[][] = [
     [8, 9, 10, 11],
     [12, 13, 14, 15],
     [16, 17, 18, 19],
-    [20, 21, 22]
+    [20, 21, 22, 23]
 ];
 
 const skillNames: string[] = [
@@ -121,7 +121,7 @@ function c(n: number) {
 */
 
 const skillProfs = [
-    5, 5, 5, 4, 4, 5, 3, 3, 5, 5, 4, 5, 4, 4, 3, 3, 4, 3, 3, 5, 3, 3, 3
+    5, 5, 5, 4, 4, 5, 3, 3, 5, 5, 4, 5, 4, 4, 3, 3, 4, 3, 3, 5, 3, 3, 3, 3
 ]
 
 function skillCircle(index: number, size: number) {
